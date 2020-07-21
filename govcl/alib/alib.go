925b8a64c2868be102fbac0308ea822b5407f309
Aquarian-Age <xiaoyaoke7630@gmail.com>
2020-07-21 21:16:27 +0800

    指定共享库目录

---
+++ govcl/alib/alib.go
@@ -0,0 +1,17 @@
+package alib
+
+import (
+	"runtime"
+
+	"github.com/ying32/govcl/pkgs/libname"
+)
+
+func init() {
+	os := runtime.GOOS
+	if os == "linux" {
+		libname.LibName = "/usr/local/lib/govcl/liblcl.so"
+	}
+	if os == "windows" {
+		libname.LibName = "C:\\ccal-dll"
+	}
+}

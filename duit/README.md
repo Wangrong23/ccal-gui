cff0aaf3a96fa5245a2911b55390ab260a50d988
Aquarian-Age <xiaoyaoke7630@gmail.com>
2020-07-24 17:38:35 +0800

    时辰汉字统一

--- duit/README.md
+++ duit/README.md
@@ -1,4 +1,11 @@
 ### ccal-duit
 
 duit写的GUI
+字体路径 /opt/fonts/
+解压unifont.tar.gz内容到上述目录
+
+```bash
+sudo mkdir /opt/fonts
+sudo tar xfv unifont.tar.gz -C /opt/fonts
+```
 
--- duit/main.go
+++ duit/main.go
@@ -3,8 +3,8 @@ package main
 import "os"
 
 func init() {
-	os.Setenv("font", "./font/unifont.font")
-	//os.Setenv("font", "/opt/fonts/unifont/unifont.font")
+	//	os.Setenv("font", "./font/unifont.font")
+	os.Setenv("font", "/opt/fonts/unifont/unifont.font")
 }
 func main() {
 	run()
